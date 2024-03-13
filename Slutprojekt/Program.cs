0d6152063bbe8c704899e16780aa1754178fa906
Haddy Sosseh <Haddy.Sosseh@elev.ga.ntig.se>
2024-03-13 18:14:59 +0100

    commit1

---
+++ Slutprojekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800,800, "Slutprojekt");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose()){
+
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.White);
+
+    Raylib.DrawRectangle(360, 700, 100, 30, Color.Black);
+
+    Raylib.EndDrawing();
+}
