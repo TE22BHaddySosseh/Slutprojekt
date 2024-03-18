240b05dd88c7c9afec35e6369c46762da6d7ca2c
Haddy Sosseh <Haddy.Sosseh@elev.ga.ntig.se>
2024-03-18 09:31:36 +0100

    commit

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,15 +1,57 @@
 ﻿using Raylib_cs;
+using System.Numerics;
+using System.Reflection.Metadata;
 
 Raylib.InitWindow(800,800, "Slutprojekt");
 Raylib.SetTargetFPS(60);
 
+Rectangle player = new Rectangle(360, 700, 100, 30);
+
+Vector2 movement = new Vector2(0,0);
+
+float speed = 5;
+
+string scene = "start";
+
 while (!Raylib.WindowShouldClose()){
+    if (scene == "game"){
+        movement = Vector2.Zero;
+
+        if (Raylib.IsKeyDown(KeyboardKey.Right)){
+            movement.X = 1;
+        }
+
+        if (Raylib.IsKeyDown(KeyboardKey.Left)){
+            movement.Y = -1;
+        }
+
+        if (movement.Length() > 0)
+        {
+            movement = Vector2.Normalize(movement) * speed;
+        }
+
+        player.X += movement.X;
+        player.Y += movement.Y;
+    }
+
+    else if (scene == "start"){
+        if (Raylib.IsKeyPressed(KeyboardKey.Space))
+        {
+            scene = "game";
+        }
+
+    }
 
     Raylib.BeginDrawing();
 
-    Raylib.ClearBackground(Color.White);
+    if (scene == "game"){
+        Raylib.ClearBackground(Color.White);
 
-    Raylib.DrawRectangle(360, 700, 100, 30, Color.Black);
+    }
+
+    else if (scene == "start"){
+
+    }
 
     Raylib.EndDrawing();
 }
